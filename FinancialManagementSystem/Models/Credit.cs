e02fbdc15f9629571d8dac7061ed0be60374e49f
Cesar <50472267+EmilCes@users.noreply.github.com>
2024-05-01 19:06:36 -0600

    Credit Service

---
+++ FinancialManagementSystem/Models/Credit.cs
@@ -0,0 +1,21 @@
+namespace FinancialManagementSystem.Models;
+
+public class Credit
+{
+    public int CreditId { get; set; }
+    public string startDate { get; set; }
+    public string endDate { get; set; }
+}
+
+
+/*
+ *  Layout
+ *  Nombre del Cliente
+ *  Fecha de Expedición
+ *  Fecha de Inicio del Crédito
+ *  Tipo de Credito
+ *  Monto Prestado
+ *  Interes
+ *  Iva
+ *  Tabla
+ */
